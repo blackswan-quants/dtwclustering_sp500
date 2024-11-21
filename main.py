7e9f6dd6360d0df7187829f7fa8cdd05dad5ec26
nicodalmonte <nicolo.dalmonte@hotmail.com>
2024-11-21 15:53:40 +0100

    hierarchical clustering with k=3,5,7,11 and silhouette index

--- main.py
+++ main.py
@@ -8,20 +8,4 @@ df_sp500.getdata()
 df_sp500.clean_df(percentage=30)
 
 
-n_series= len(df_sp500)
-# Initializing distance matrix
-euclidean_distance = np.zeros((n_series, n_series))
-dtw0_distance = np.zeros((n_series, n_series))
-dtw60_distance = np.zeros((n_series, n_series))
 
-#two for cycle in order to obtain a distance matrix
-for i in range(n_series):
-    for j in range(i, n_series):
-        distance= DTWClustering(df_sp500.iloc[i], df_sp500.iloc[j])
-        euclidean_distance[i,j]= distance.euclidean_distance()
-        dtw0_distance[i,j]= distance.dtw_no_window()
-        dtw60_distance[i,j]= distance.dtw_with_window()
-        # copy to value because the matrix is symmetric
-        euclidean_distance[j, i] = euclidean_distance[i, j]
-        dtw0_distance[j, i] = dtw0_distance[i, j]
-        dtw60_distance[j, i] = dtw60_distance[i, j]
