6d3e5b9d3f3c6d990e127a127ac4252a9671a6b1
giutala <giuliatala32@gmail.com>
2024-12-22 22:22:39 +0100

    - added parent folder to pickle files - updated functions to treat df.T - rewrote plotting - handling weekly case now

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from helpermodules.df_dataretrieval import IndexData_Retrieval
-from helpermodules.DTWclustering import DTWClustering
+from helpermodules.DTWClustering import DTWClustering
 import numpy as np
 import pandas as pd
 
-df_sp500 = IndexData_Retrieval(filename='sp500_df', index='S&P 500',years=10, frequency='1d', use_yfinance=True )
+df_sp500 = IndexData_Retrieval(filename='sp500_weekly', index='S&P 500',years=10, frequency='5d', use_yfinance=True )
 df_sp500.getdata()
 df_sp500.clean_df(percentage=30)
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+df_sp500.getdata()
